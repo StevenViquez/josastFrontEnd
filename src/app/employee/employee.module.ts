1391f8a4a55588c305fb0fad4418ebcd44ec5554
Steven Viquez Benavides <sviquezb@cisco.com>
2020-11-19 23:42:43 -0600

    +Added Employee componenet: create and update
    +Added Product componenet: create and update

--- src/app/employee/employee.module.ts
+++ src/app/employee/employee.module.ts
@@ -5,10 +5,12 @@ import { EmployeeRoutingModule } from './employee-routing.module';
 import { EmployeeAllComponent } from './employee-all/employee-all.component';
 import { EmployeeIndexComponent } from './employee-index/employee-index.component';
 import { EmployeeShowComponent } from './employee-show/employee-show.component';
+import { EmployeeCreateComponent } from './employee-create/employee-create.component';
+import { EmployeeUpdateComponent } from './employee-update/employee-update.component';
 
 
 @NgModule({
-  declarations: [EmployeeAllComponent, EmployeeIndexComponent, EmployeeShowComponent],
+  declarations: [EmployeeAllComponent, EmployeeIndexComponent, EmployeeShowComponent, EmployeeCreateComponent, EmployeeUpdateComponent],
   imports: [
     CommonModule,
     EmployeeRoutingModule
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -5,10 +5,12 @@ import { ProductRoutingModule } from './product-routing.module';
 import { ProductAllComponent } from './product-all/product-all.component';
 import { ProductIndexComponent } from './product-index/product-index.component';
 import { ProductShowComponent } from './product-show/product-show.component';
+import { ProductCreateComponent } from './product-create/product-create.component';
+import { ProductUpdateComponent } from './product-update/product-update.component';
 
 
 @NgModule({
-  declarations: [ProductIndexComponent, ProductAllComponent, ProductShowComponent],
+  declarations: [ProductIndexComponent, ProductAllComponent, ProductShowComponent, ProductCreateComponent, ProductUpdateComponent],
   imports: [
     CommonModule,
     ProductRoutingModule
